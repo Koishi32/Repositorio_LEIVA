bf5f18490f1f4c8eb6a0cd8a46088d3f724bd27c
Diaz <alejandroivanleyvadiaz@gmail.com>
2019-09-08 18:03:56 -0500

    ModificacionUI
    Se agregaron cosas al UI e incluyo el escript de regreso a main menu

---
+++ Prototipado/Assets/LEIVA/scripts/DungeonManagement/ReturnMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ReturnMenu : MonoBehaviour
+{
+    public float tiemposo_limit; // tiempo en segundos para que sea return
+    public float tiempo_actual;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey || Input.GetAxis("Mouse Y") !=0 || Input.GetAxis("Mouse X") !=0) {
+            tiempo_actual = 0;
+        }
+        if (tiemposo_limit < tiempo_actual) {
+            SceneManager.LoadScene(1); // se carga la escena del menu principal
+        }
+        tiempo_actual += Time.deltaTime;
+    }
+}
